a265ebc0565435f469955933d486ee7f9e5f4c43
Asadsheblu <shebluasad@gmail.com>
2022-05-24 03:10:37 +0600

    admin-explore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const app=express()
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const cors=require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -8,6 +8,23 @@ const { query } = require('express')
 app.use(cors())
 const port = process.env.PORT || 5000;
 app.use(express.json());
+function verifyJWT (req,res,next){
+    const authHeader=req.headers.authorization
+    if(!authHeader){
+        return res.status(401).send({message:"Unauthorized access"})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        
+        req.decoded = decoded;
+        next();
+    })
+    
+}
+
 app.get('/',(req,res)=>{
     res.send("Hello Bicyle-parts-manufacture backend sever")
 })
@@ -18,6 +35,19 @@ async function run() {
     try {
         await client.connect();
         const collection=client.db("bicyle-parts").collection("parts")
+        const userInfo=client.db("bicyle-parts").collection("userInfo")
+        const userCollection=client.db("bicyle-parts").collection("user")
+        const reviewCollection=client.db("bicyle-parts").collection("review")
+        const productCollection=client.db("bicyle-parts").collection("product")
+        //verifyAdmin
+const verifyAdmin=async(req,res,next)=>{
+    const requester=req.decoded.email
+const requesterAccount=await userCollection.findOne({email:requester})
+if(requesterAccount.role=="admin"){
+next()
+
+}
+}
         //get api for parts
        app.get('/parts',async(req,res)=>{
         const query={}
@@ -26,11 +56,87 @@ async function run() {
         res.send(parts)
        })
        app.get('/parts/:id',async(req,res)=>{
-        const query={}
-        const result=collection.findOne(query)
-        
+        const id=(req.params.id);
+        const query={_id:ObjectId(id)}
+        const result=await collection.findOne(query)
         res.send(result)
-       })
+    })
+       //user info api create
+       app.post('/info',async(req,res)=>{
+        const doc =req.body;
+        const result=await userInfo.insertOne(doc)
+        res.send(result)
+        
+    })
+       //user info api get
+       //my item
+       app.get('/userinfo', async (req, res) => {
+        const query = {};
+        const cursor = userInfo.find(query);
+        const profile = await cursor.toArray();
+        res.send(profile);
+    })
+ //put method for add user
+ app.put('/user/:email',async(req,res)=>{
+    const email=req.params.email
+    const user=req.body
+    const filter = { email: email };
+    const options = { upsert: true };
+    const updateDoc = {
+        $set: user
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1y' })
+      res.send({ result, token });
+   })
+//    //admin api
+   app.get('/admin/:email',async(req,res)=>{
+    const email=req.params.email 
+    const user=await userCollection.findOne({email:email})
+    const isAdmin=user.role==="admin"
+    res.send({admin:isAdmin})
+   })
+   //put method for add admin
+   app.put('/user/admin/:email',verifyJWT,verifyAdmin,async(req,res)=>{
+    const email=req.params.email
+
+    const filter = { email: email };
+    
+    const updateDoc = {
+        $set: {role:"admin"}
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      
+      res.send({ result });
+
+   })
+   //get
+app.get('/user',verifyJWT,async(req,res)=>{
+    const query=req.params.email
+        const result=userCollection.find(query)
+        const user=await result.toArray()
+        res.send(user)
+})
+//review add
+app.post('/review',async(req,res)=>{
+    const doc =req.body;
+    const result=await reviewCollection.insertOne(doc)
+    res.send(result)
+    
+})
+app.get('/review',async(req,res)=>{
+    const query={}
+        const result=reviewCollection.find(query)
+        const user=await result.toArray()
+        res.send(user)
+})
+//add Product
+app.post('/parts',verifyJWT,verifyAdmin,async(req,res)=>{
+    const doctor =req.body;
+    const result=await collection.insertOne(doctor)
+    res.send(result)
+    
+})
 
     }
     finally{
