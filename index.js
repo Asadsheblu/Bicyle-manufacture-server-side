4c6ba25db6fd52a2dd49ee86bc0d8086564caf67
Asadsheblu <shebluasad@gmail.com>
2022-05-26 23:35:24 +0600

    added

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ require('dotenv').config()
 const cors=require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require('express')
-app.use(cors())
-const port = process.env.PORT || 5500;
-app.use(express.json());
+// app.use(cors())
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+app.use(express.json())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
     next()
 })
+const port = process.env.PORT || 5000;
+// app.use(express.json());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next()
+// })
+
 function verifyJWT (req,res,next){
     const authHeader=req.headers.authorization
     if(!authHeader){
